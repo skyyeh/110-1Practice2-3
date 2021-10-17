446c12c25b74b206e76480ed72e855020d2c7631
于翔 林 <108111120@mail.aeust.edu.tw>
2021-10-17 10:33:29 +0800

    新增Nar

---
+++ 110-1Practice2-3/Nar.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Nar.aspx.cs" Inherits="_110_1Practice2_3.Nar" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ 110-1Practice2-3/Nar.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _110_1Practice2_3
+{
+    public partial class Nar : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int i_n = 153;
+            int N = 1;
+            int rm = 10;
+            for (int i = 0; i < N; i++)
+            {
+                if (i_n / rm > 0)
+                {
+                    N += 1;
+                    rm = rm * 10;
+                }
+            }
+
+            int i_a = i_n / 100;
+            int i_b = (i_n % 100) / 10;
+            int i_c = i_n % 10;
+
+            if (Math.Pow(i_a, N) + Math.Pow(i_b, N) + Math.Pow(i_c, N) == i_n)
+            {
+                Response.Write("True");
+            }
+            else
+            {
+                Response.Write("False");
+            }
+
+
+        }
+    }
+}
