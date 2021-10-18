eccb8f4f565c03514a88a9cc3b037c9b9bad876b
于翔 林 <108111120@mail.aeust.edu.tw>
2021-10-18 19:31:09 +0800

    新增Nar

--- 110-1Practice2-3/Nar.aspx.cs
+++ 110-1Practice2-3/Nar.aspx.cs
@@ -9,33 +9,34 @@ namespace _110_1Practice2_3
 {
     public partial class Nar : System.Web.UI.Page
     {
-
         protected void Page_Load(object sender, EventArgs e)
         {
-            int i_n = 153;
-            int N = 1;
-            int rm = 10;
-            for (int i = 0; i < N; i++)
+            int num = 153;  
+            int num_new, rem, sum = 0; 
+
+            num_new = num;
+
+            while (num_new != 0)
             {
-                if (i_n / rm > 0)
-                {
-                    N += 1;
-                    rm = rm * 10;
-                }
+                rem = num_new % 10; 
+                sum = sum + (rem * rem * rem); 
+                num_new = num_new / 10; 
             }
+            
 
-            int i_a = i_n / 100;
-            int i_b = (i_n % 100) / 10;
-            int i_c = i_n % 10;
-
-            if (Math.Pow(i_a, N) + Math.Pow(i_b, N) + Math.Pow(i_c, N) == i_n)
+            if (sum == num)
             {
                 Response.Write("True");
             }
             else
             {
-                Response.Write("False");
+                Response.Write("Flase");
             }
+             
+
+
+
+
 
 
         }
